14c32a0229860f0952fafeefb1ef1b66e0ad38bb
Marc Vorreiter <vorreiter@fh-luebeck.de>
2013-07-09 14:56:52 +0200

    bug109 bug121 added new area types

--- LoopArea.php
+++ LoopArea.php
@@ -34,7 +34,7 @@ class LoopArea {
 	function LoopArea($input,$args) {
 		global $wgParser, $wgTitle, $wgParserConf, $wgUser, $wgLoopAreaDefaultRenderOption;
 
-		$areatypes=array('task','timerequirement','learningobjectives','arrangement','example','reflection','notice','sourcecode','summary','important','markedsentence','annotation','definition','formula','indentation','area','norm','law');
+		$areatypes=array('task','timerequirement','learningobjectives','arrangement','example','reflection','notice','sourcecode','summary','important','markedsentence','annotation','definition','formula','indentation','area','norm','law','question','practice','exercise','websource','experiment');
 
 		$this->input=trim($input);
 		$this->args=$args;
