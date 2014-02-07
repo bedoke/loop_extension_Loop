8069e80b48d478a0b765210a57725cdf4b421603
Marc Vorreiter <vorreiter@fh-luebeck.de>
2014-02-07 15:19:02 +0100

    fixed bug270 added links on index page

--- SpecialLoopIndex.php
+++ SpecialLoopIndex.php
@@ -12,27 +12,60 @@ class SpecialLoopIndex extends SpecialPage {
 		$wgOut->addModules( 'ext.LoopIndex' );
 		$this->setHeaders();
 
+		setlocale(LC_CTYPE, 'cs_CZ');
+		
 		$indexitems=array();
 		$return = '<h1>'.wfMsg('loopindex').'</h1>';
 
 		$sql="select indexes.in_title, page.page_id, page.page_title, loopstructure.TocNumber from (indexes left join page on indexes.in_from=page.page_id) left join loopstructure on page.page_id= loopstructure.ArticleId  order by LOWER(indexes.in_title), loopstructure.TocNumber";
 		$dbr = wfGetDB( DB_SLAVE );
 		$res = $dbr->query( $sql, __METHOD__, true );
-
+		
+		$act_index_title='';
+		$act_index_letter='';		
+		$letters=array();
+		foreach ( $res as $row ) {
+			$act_letter = mb_substr($row->in_title,0,1);
+			$act_letter = iconv('UTF-8', 'US-ASCII//TRANSLIT', $act_letter);
+			if ((ord($act_letter)>=48) && (ord($act_letter)<=57)) {
+				$act_letter = '#';
+			}
+			if ($act_letter!=$act_index_letter) {
+				$act_index_letter=$act_letter;
+				$letters[] = $act_letter;
+			}
+		}
+		$allletters=array('#','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+		
+		$return.= '<table><tr>';
+		foreach ( $allletters as $letter ) {
+			if (in_array($letter,$letters)) {
+				$return.='<td><a href="#indexletter_'.ord($letter).'">'.$letter.'</a></td>';
+			} else {
+				$return.='<td>'.$letter.'</td>';
+			}
+		}
+		$return.= '</tr></table>';
+		
 		$act_index_title='';
 		$act_index_letter='';
 		$return.='<div id="loopindex"><table>';
 		$first=true;
 		$closed=false;
 		foreach ( $res as $row ) {
-			if (substr($row->in_title,0,1)!=$act_index_letter) {
-				$act_index_letter=substr($row->in_title,0,1);
+			$act_letter = mb_substr($row->in_title,0,1);
+			$act_letter = iconv('UTF-8', 'US-ASCII//TRANSLIT', $act_letter);		
+			if ((ord($act_letter)>=48) && (ord($act_letter)<=57)) {
+				$act_letter = '#';
+			}
+			if ($act_letter!=$act_index_letter) {
+				$act_index_letter=$act_letter;
 				if ((!$first)&&(!closed)) {
 					$return.= '</td></tr>';
 					$closed=true;
 				}				
 				
-				$return.= '<tr><td><div class="loopindex_letter">'.$act_index_letter.'</div></td><td>&nbsp;</td></tr>';
+				$return.= '<tr><td><div class="loopindex_letter" id="indexletter_'.ord($act_index_letter).'">'.$act_index_letter.'</div></td><td>&nbsp;</td></tr>';
 			}
 			
 			//var_dump($row);
